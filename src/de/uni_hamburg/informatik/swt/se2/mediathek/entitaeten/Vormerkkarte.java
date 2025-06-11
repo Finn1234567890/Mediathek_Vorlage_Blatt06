24a4a1544e784d24fbf614425abf6090c1f3dd7a
Finn Langenkaemper <finnsebastianl@gmail.com>
2025-06-11 09:20:17 +0200

    Initial commit

---
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/entitaeten/Vormerkkarte.java
@@ -0,0 +1,112 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.entitaeten;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.entitaeten.medien.Medium;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Mit Hilfe von Vormerkkarten werden beim Vormerken eines Mediums alle relevanten
+ * Daten notiert.
+ * 
+ * Sie beantwortet die folgenden Fragen: Welches Medium wurde vorgemerkt? Wer
+ * hat das Medium bereits vorgemerkt?
+ * 
+ *  Um die Verwaltung der Karten kümmert sich der VerleihService
+ * 
+ * @author Finn-Malte-Schmiddy
+ * @version SoSe 2025
+ */
+public class Vormerkkarte
+{
+
+    // Eigenschaften einer Vormerkkarte
+    private final Queue<Kunde> _vormerker;
+    private final Medium _medium;
+
+    /**
+     * Initialisert eine neue Vormerkkarte mit den gegebenen Daten.
+     *
+     * @param medium Das Medium fuer die Vormerkkarte
+     *
+     * @require medium != null
+     *
+     * @ensure #getMedium() == medium
+     */
+    public Vormerkkarte(Medium medium)
+    {
+        assert medium != null : "Vorbedingung verletzt: medium != null";
+
+        _medium = medium;
+        _vormerker = new LinkedList<Kunde>();
+    }
+
+    /**
+     * Gibt ArrayList aller Vormerker aus.
+     *
+     * @return Liste aller vormerker Kunden
+     *
+     */
+    public List<Kunde> getVormerker() {
+        List<Kunde> vormerkerList = new ArrayList<>();
+        for (Kunde kunde: _vormerker) {
+           vormerkerList.add(kunde);
+        }
+
+        return vormerkerList;
+    }
+
+    /**
+     * Fuege Vormker am Ende der Vormerker Queue hinzu
+     *
+     * @param kunde der Kunde der sich Vormerken will
+     */
+    public void addVormerker(Kunde kunde) {
+        _vormerker.add(kunde);
+        System.out.println("vormerker added");
+        System.out.println(_vormerker);
+    }
+
+    /**
+     * Entfernt ersten Vormerker aus der Queue (FiFo)
+     *
+     */
+    public void entferneOberstenVormerker() {
+        if (!_vormerker.isEmpty()) {
+            _vormerker.remove();
+        }
+    }
+
+    /**
+     * Gibt den ersten Vormerker zurueck
+     *
+     * @return der ersten Vormerker aus der queue
+     *
+     */
+    public Kunde getErstenVormerker() {
+        return _vormerker.peek();
+    }
+
+    /**
+     * Prueft ob das Vormerken moeglich ist anhand davon ob es weniger als 3 vormerker gibt
+     *
+     * @return true oder false ob vormerken moeglich ist
+     */
+    public boolean istVormerkenMoeglich() {
+        return _vormerker.size() < 3;
+    }
+
+    /**
+     * Gibt das Medium, dessen Ausleihe auf der Karte vermerkt ist, zurück.
+     * 
+     * @return Das Medium, dessen Ausleihe auf dieser Karte vermerkt ist.
+     * 
+     * @ensure result != null
+     */
+    public Medium getMedium()
+    {
+        return _medium;
+    }
+}
---
+++ src/de/uni_hamburg/informatik/swt/se2/mediathek/entitaeten/VormerkkarteTest.java
@@ -0,0 +1,110 @@
+package de.uni_hamburg.informatik.swt.se2.mediathek.entitaeten;
+
+import de.uni_hamburg.informatik.swt.se2.mediathek.entitaeten.medien.CD;
+import de.uni_hamburg.informatik.swt.se2.mediathek.entitaeten.medien.Medium;
+import de.uni_hamburg.informatik.swt.se2.mediathek.wertobjekte.Kundennummer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
+/*
+ * Testklasse für die Vormerkkarte
+ */
+public class VormerkkarteTest
+{
+    private final Medium _medium;
+    private final Kunde _kunde1;
+    private final Kunde _kunde2;
+    private final Kunde _kunde3;
+    private final Kunde _kunde4;
+    private final Vormerkkarte _vormerkkarte;
+
+    public VormerkkarteTest()
+    {
+        _medium = new CD("Scream", "Geil", "Ghost", 1); // Medium erstellen
+
+        // Kunden erstellen
+        _kunde1 = new Kunde(new Kundennummer(111111), "Nico", "Schmidt");
+        _kunde2 = new Kunde(new Kundennummer(222222), "Finn", "Kämper");
+        _kunde3 = new Kunde(new Kundennummer(333333), "Malte", "Engel");
+        _kunde4 = new Kunde(new Kundennummer(444444), "Max", "Abe");
+        
+        _vormerkkarte = new Vormerkkarte(_medium); // Vormerkkarte erstellen
+        _vormerkkarte.addVormerker(_kunde1);
+    }
+
+
+    @Test
+    public void TestIstVormerkenMoeglich()
+    {
+        _vormerkkarte.addVormerker(_kunde2);
+        _vormerkkarte.addVormerker(_kunde3);
+
+        assertFalse(_vormerkkarte.istVormerkenMoeglich(_kunde3)); //Vormerker darf nicht 2 mal auf Vormerkliste stehen
+        assertFalse(_vormerkkarte.istVormerkenMoeglich(_kunde4)); //nicht mehr als 3 Vormerker
+    }
+
+    @Test
+    public void TestMerkeVor()
+    {
+        _vormerkkarte.merkeVor(_kunde2);
+        _vormerkkarte.merkeVor(_kunde3);
+
+        assertTrue(_vormerkkarte.getVormerkerListe().contains(_kunde2));
+        assertTrue(_vormerkkarte.getVormerkerListe().contains(_kunde1));
+    }
+
+    @Test
+    public void TestGetErstenVormerker()
+    {
+        _vormerkkarte.merkeVor(_kunde2);
+        _vormerkkarte.merkeVor(_kunde3);
+
+        assertEquals(_kunde1, _vormerkkarte.gibErstenVormerker());
+
+        _vormerkkarte.entferneVormerker();
+        assertEquals(_kunde2, _vormerkkarte.gibErstenVormerker());
+    }
+
+    @Test
+    public void TestGetMedium()
+    {
+        assertEquals(_medium, _vormerkkarte.getMedium());
+    }
+
+    @Test
+    public void TestEntferneOberstenVormerker()
+    {
+        _vormerkkarte.merkeVor(_kunde2);
+        _vormerkkarte.entferneVormerker();
+        assertFalse(_vormerkkarte.getVormerkerListe().contains(_kunde1));
+        assertEquals(_kunde2, _vormerkkarte.gibErstenVormerker());
+        assertTrue(_vormerkkarte.getVormerkerListe().contains(_kunde2));
+    }
+
+    @Test
+    public void TestGetVormerker()
+    {
+        assertEquals(_kunde1, _vormerkkarte.getVormerker(0));
+
+        _vormerkkarte.merkeVor(_kunde2);
+        _vormerkkarte.merkeVor(_kunde3);
+
+        assertEquals(_kunde2, _vormerkkarte.getVormerker(1));
+        assertEquals(_kunde3, _vormerkkarte.getVormerker(2));
+    }
+    
+    @Test
+    public void TestAddVormerker()
+    {
+    	//kunde 1 nicht weil der bei initalisierung schon drin war
+    	_vormerkkarte.addVormerker(_kunde2);
+        _vormerkkarte.addVormerker(_kunde3);
+
+        // korrekt vorgemerkt?
+        assertEquals(_kunde1, _vormerkkarte.getVormerker(0));
+        assertEquals(_kunde2, _vormerkkarte.getVormerker(1));
+        assertEquals(_kunde3, _vormerkkarte.getVormerker(2));
+    }
+
+}
